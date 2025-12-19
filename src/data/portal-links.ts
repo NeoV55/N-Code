38ce0289f9aed141bad2ac65616511d0f3278596
Neo555 <96537585+NeoV55@users.noreply.github.com>
2025-12-19 23:49:09 +0800

    Update portal-links.ts

--- src/data/portal-links.ts
+++ src/data/portal-links.ts
@@ -1,9 +1,9 @@
 export const PORTAL_LINKS = {
     // Using the same editable Google Form URL for mock/demo purposes
-    parentConsent: "https://docs.google.com/forms/d/e/1FAIpQLSea-K_aRMSJnL10XKisu1aXiNN_1EBruZIUqPxBmYjWCPlVZQ/viewform",
-    terms: "https://docs.google.com/forms/d/e/1FAIpQLScevIAMRkTrEI-0JMu6MNsV1bBbIW5iTg_1Nd-L5cgWtQcvGA/viewform",
+    "parentConsent": "https://docs.google.com/forms/d/e/1FAIpQLSea-K_aRMSJnL10XKisu1aXiNN_1EBruZIUqPxBmYjWCPlVZQ/viewform",
+    "terms": "https://docs.google.com/forms/d/e/1FAIpQLScevIAMRkTrEI-0JMu6MNsV1bBbIW5iTg_1Nd-L5cgWtQcvGA/viewform",
     "student-registration": "https://docs.google.com/forms/d/e/1FAIpQLSdplQXpZ7PgxLFkMG1WtdSGkb6Phz3gDjdIlbHY5br0q-VVrw/viewform",
-    support: "mailto:support@n-code.com",
+    "support": "mailto:support@n-code.com",
 };
 
 export const PORTAL_NAV = [
