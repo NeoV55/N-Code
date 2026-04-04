bff371444685661bd260827d292e3724d4d2337a
Neo555 <96537585+NeoV55@users.noreply.github.com>
2026-04-04 23:06:07 +0800

    Change button text from 'Resume' to 'Cv'

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -102,7 +102,7 @@ const HeroSection = () => {
                   <BoxReveal delay={2} width="100%" >
                     <Button className="flex items-center gap-2 w-full">
                       <File size={24} />
-                      <p>Resume</p>
+                      <p>Cv</p>
                     </Button>
                   </BoxReveal>
                 </Link>
